291f3ec343b7b080dde6f76499c915dc66d1ce3a
Bao Vuong <vqbao@lv5.org>
2019-10-14 19:28:26 -0400

    Add project files.

---
+++ PassTheCopyPastaBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using System;
+using System.Linq;
+
+namespace PassTheCopyPastaBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetRandomPost().Data.Selftext);
+        }
+
+        static Child GetRandomPost(string type = "top")
+        {
+            return GetPosts(type, 100).Data.Children
+                .AsEnumerable()
+                .Where(c => c.Data.Selftext.Length > 0)
+                .RandomShuffle()
+                .FirstOrDefault();
+        }
+
+        static Posts GetPosts(string type = "top", int limit = 100)
+        {
+            var client = new RestClient("https://www.reddit.com");
+
+            var request = new RestRequest("r/copypasta/" + type + ".json", Method.GET);
+            request.AddQueryParameter("limit", limit.ToString());
+            request.AddQueryParameter("t", "all");
+
+            var response = client.Execute(request);
+
+            return Posts.FromJson(response.Content);
+        }
+    }
+}
