f4d9ef1c0d7eb151ce1e3c42b0a466fca7276fcf
Bao Vuong <vqbao@lv5.org>
2019-10-17 11:38:56 -0400

    got first stuff in

---
+++ PassTheCopyPastaBot/PostFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace PassTheCopyPastaBot
+{
+    public enum PostFilter
+    {
+        TOP,
+        HOT,
+        NEW,
+        INVALID
+    }
+
+    static partial class PostFilterHelper
+    {
+        private static readonly IDictionary<PostFilter, string> mapping = new Dictionary<PostFilter, string>
+        { 
+            {PostFilter.TOP, "top" },
+            {PostFilter.HOT, "hot" },
+            {PostFilter.NEW, "new" }
+        };
+
+        public static string GetString(this PostFilter postFilter)
+        {
+            string value;
+
+            if (mapping.TryGetValue(postFilter, out value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+    }
+
+}
--- PassTheCopyPastaBot/Program.cs
+++ PassTheCopyPastaBot/Program.cs
@@ -1,36 +1,80 @@
-﻿using RestSharp;
+﻿using Microsoft.Extensions.Configuration;
+using RestSharp;
 using System;
+using System.IO;
 using System.Linq;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace PassTheCopyPastaBot
 {
     class Program
     {
+        static ITelegramBotClient botClient;
+
         static void Main(string[] args)
         {
-            Console.WriteLine(GetRandomPost().Data.Selftext);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            botClient = new TelegramBotClient(configuration.GetSection("Settings").GetSection("TelegramBotToken").Value);
+
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
         }
 
-        static Child GetRandomPost(string type = "top")
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
         {
-            return GetPosts(type, 100).Data.Children
-                .AsEnumerable()
-                .Where(c => c.Data.Selftext.Length > 0)
-                .RandomShuffle()
-                .FirstOrDefault();
-        }
+            var pasta = new CopyPasta { CharacterLimit = 4000 };
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}: {e.Message.Text}");
+                PostFilter postFilter;
+                switch (e.Message.Text.ToLower())
+                {
+                    case "/top":
+                        Console.WriteLine($"Sending Top Copypasta to {e.Message.Chat.Id}.");
+                        postFilter = PostFilter.TOP;
+                        break;
+                    case "/hot":
+                        Console.WriteLine($"Sending Hot Copypasta to {e.Message.Chat.Id}.");
+                        postFilter = PostFilter.HOT;
+                        break;
+                    case "/new":
+                        Console.WriteLine($"Sending New Copypasta to {e.Message.Chat.Id}.");
+                        postFilter = PostFilter.NEW;
+                        break;
+                    default:
+                        Console.WriteLine($"Unknown command from {e.Message.Chat.Id}.");
+                        postFilter = PostFilter.INVALID;
+                        break;
+                }
 
-        static Posts GetPosts(string type = "top", int limit = 100)
-        {
-            var client = new RestClient("https://www.reddit.com");
+                if (postFilter != PostFilter.INVALID)
+                {
+                    var post = pasta.GetRandomPasta(postFilter);
+                    await botClient.SendTextMessageAsync(
+                        chatId: e.Message.Chat,
+                        text: post.Selftext,
+                        replyMarkup: new InlineKeyboardMarkup(new[]
+                        {
+                            InlineKeyboardButton.WithUrl("Go to Post", post.Url.ToString())
+                        })
+                    );
 
-            var request = new RestRequest("r/copypasta/" + type + ".json", Method.GET);
-            request.AddQueryParameter("limit", limit.ToString());
-            request.AddQueryParameter("t", "all");
-
-            var response = client.Execute(request);
-
-            return Posts.FromJson(response.Content);
+                }
+            }
         }
     }
 }
